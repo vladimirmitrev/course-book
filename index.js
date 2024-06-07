81155530bc29b694bb860b6199e44d02eb395275
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-07 09:59:42 +0300

    Fix pretier extension to use single quotes

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const express = require("express");
+const express = require('express');
+const routes = require('routes');
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(3000, () =>
-  console.log("App is listening on http://localhost:3000")
+  console.log('App is listening on http://localhost:3000')
 );
