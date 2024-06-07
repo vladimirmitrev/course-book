46508a15a77ce3a7cdd24c5aa91d1d80da56c7bd
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-07 10:08:03 +0300

    Add routes module

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const routes = require('routes');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
 
 app.listen(3000, () =>
   console.log('App is listening on http://localhost:3000')
