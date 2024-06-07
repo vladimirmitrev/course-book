d9a88911563cbd21b1cf970b522bb75988c240c0
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-07 09:52:27 +0300

    Config express, body parser

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(3000, () => console.log('App is listening on http://localhost:3000'));
+app.listen(3000, () =>
+  console.log("App is listening on http://localhost:3000")
+);
