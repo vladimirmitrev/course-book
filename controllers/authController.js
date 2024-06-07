00ff7b2ad956f1ae45e71702e8e3e50518211b21
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-07 11:20:42 +0300

    Add home and auth controllers and pages

---
+++ controllers/authController.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.get('/register', (req, res) => {
+  res.render('auth/register');
+});
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+  });
+  
+
+module.exports = router;
