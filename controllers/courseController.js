9121360088719e133fe73e5326ae4e9cfaae0f2a
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-08 14:52:18 +0300

    Add sign up users and displayed signed up to course

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -14,13 +14,28 @@ router.get('/:courseId/details', async (req, res) => {
     const courseId = req.params.courseId;
     try {
       const course = await courseService.getOneDetailed(courseId).lean();
+      const signedUpUsers = course.singUpList.map(user => user.username).join(', ');
 
-      res.render('courses/details', { ...course });
+      res.render('courses/details', { ...course, signedUpUsers });
     } catch (err) {
       // console.log(err);
       res.redirect('/');
     }
     
+});
+
+router.get('/:courseId/sign-up', async (req, res) => {
+  // const courseId = req.params.courseId;
+  try {
+    await courseService.singUp(req.params.courseId, req.user._id);
+
+    res.redirect(`/courses/${req.params.courseId}/details`);
+
+  } catch (err) {
+    // console.log(err);
+    res.redirect('/');
+  }
+
 });
 
 router.get('/create', isAuth, (req, res) => {
--- services/courseService.js
+++ services/courseService.js
@@ -16,4 +16,19 @@ exports.getAll = () => Course.find();
 
 exports.getOne = (courseId) => Course.findById(courseId);
 
-exports.getOneDetailed = (courseId) => this.getOne(courseId).populate('owner');
+exports.getOneDetailed = (courseId) => this.getOne(courseId).populate('owner').populate('singUpList');
+
+exports.singUp = async (courseId, userId) => {
+    //With 2 queries
+    // await Course.findByIdAndUpdate(courseId, { push$: { singUpList: userId } });
+    // await User.findByIdAndUpdate(userId, { push$: { singedUpCourses: courseId } });
+
+    const course = await Course.findById(courseId);
+    const user = await User.findById(userId);
+
+    course.singUpList.push(userId);
+    user.singedUpCourses.push(courseId);
+
+    await course.save();
+    await user.save();
+};
