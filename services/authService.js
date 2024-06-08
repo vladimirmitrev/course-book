fc3a8ef0eb2e8be8a91ab529a3fc6d498bb1ce6c
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-08 15:33:12 +0300

    Remove bug when login

--- services/authService.js
+++ services/authService.js
@@ -27,7 +27,7 @@ exports.login = async (email, password) => {
     throw new Error('Invalid email or password!');
   }
 
-  const isValid = await bcrypt.compare(password, user.password);
+  const isValid = bcrypt.compare(password, user.password);
   if (!isValid) {
     throw new Error('Invalid email or password!');
   }
