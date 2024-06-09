8b93154d79dc22901fe937b8f2f5bd2208f0f35d
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-06-09 13:45:42 +0300

    Add profile page and show profile info

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { isAuth } = require('../middlewares/authMiddleware');
 const courseService = require('../services/courseService');
+const userService = require('../services/userService');
 
 router.get('/', async (req, res) => {
   const latestCourses = await courseService.getLatest().lean();
@@ -8,6 +9,14 @@ router.get('/', async (req, res) => {
   res.render('home', { latestCourses });
 });
 
+router.get('/profile', isAuth, async (req, res) => {
+  const user = await userService.getInfo(req.user._id).lean();
+
+  const createdCoursesCount = user.createdCourses.length;
+  const singedUpCoursesCount = user.singedUpCourses.length;
+  
+  res.render('profile', { user, createdCoursesCount, singedUpCoursesCount });
+});
 // router.get('/test', isAuth, (req, res) => {
 //   res.send('You are authorized')
 // })
